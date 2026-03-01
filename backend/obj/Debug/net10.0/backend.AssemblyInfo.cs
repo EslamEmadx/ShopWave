048d0da4d5839267eed834fb2425c3eed2710da4
EslamEmadx <islamemadtwab@gmail.com>
2026-03-01 03:49:14 +0200

    feat: Add initial backend build artifacts, log file, frontend environment configuration, and API service.

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c42d7afaa10cdc11770abc53c36a286f1ccf3846")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6397a1b4e34df25cd1be3e974eca21df641b3f68")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
